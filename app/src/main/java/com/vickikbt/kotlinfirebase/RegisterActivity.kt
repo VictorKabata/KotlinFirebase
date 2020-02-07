0ea1b6d203e36607e5508e45263c8c14c9330991
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-07 20:51:54 +0300

    Added user registration Kotlin Code.

--- app/src/main/java/com/vickikbt/kotlinfirebase/RegisterActivity.kt
+++ app/src/main/java/com/vickikbt/kotlinfirebase/RegisterActivity.kt
@@ -2,14 +2,17 @@ package com.vickikbt.kotlinfirebase
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.vickikbt.kotlinfirebase.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRegisterBinding
+    val firebaseAuth=FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,17 +28,27 @@ class RegisterActivity : AppCompatActivity() {
     }
 
     private fun registerUsers() {
-        val username = binding.usernameEditText.text
-        val email = binding.emailEditText.text
-        val password = binding.passwordEdiText.text
+        val username = binding.usernameEditText.text.toString()
+        val email = binding.emailEditText.text.toString()
+        val password = binding.passwordEdiText.text.toString()
 
         when {
-            username!!.isEmpty() -> Toast.makeText(this, "Enter Username!", Toast.LENGTH_SHORT).show()
-            email!!.isEmpty() -> Toast.makeText(this, "Enter Email!", Toast.LENGTH_SHORT).show()
-            password!!.isEmpty() -> Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show()
+            username.isEmpty() -> Toast.makeText(this, "Enter Username!", Toast.LENGTH_SHORT).show()
+            email.isEmpty() -> Toast.makeText(this, "Enter Email!", Toast.LENGTH_SHORT).show()
+            password.isEmpty() -> Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show()
             password.length<=8 -> Toast.makeText(this, "Password is too short!", Toast.LENGTH_SHORT).show()
-
         }
+
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(!it.isSuccessful){
+                    Toast.makeText(this, "Registration Failed!", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "Registration Successful!", Toast.LENGTH_SHORT).show()
+                    Log.i("Registration", "User created successfully: ${it.result?.user?.uid}")
+                }
+            }
 
     }
 }
