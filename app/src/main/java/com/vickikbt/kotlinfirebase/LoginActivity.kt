2b28bc6c026177aa01bd712f2dcdefa8411304bb
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-07 18:55:47 +0300

    Added onClickListener to switch activities.

--- app/src/main/java/com/vickikbt/kotlinfirebase/LoginActivity.kt
+++ app/src/main/java/com/vickikbt/kotlinfirebase/LoginActivity.kt
@@ -1,17 +1,22 @@
 package com.vickikbt.kotlinfirebase
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.vickikbt.kotlinfirebase.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    
+
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+        binding.goToRegisterTextView.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
 
 
     }
--- app/src/main/java/com/vickikbt/kotlinfirebase/RegisterActivity.kt
+++ app/src/main/java/com/vickikbt/kotlinfirebase/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.vickikbt.kotlinfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -12,5 +13,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=DataBindingUtil.setContentView(this, R.layout.activity_register)
+
+        binding.goToLoginTextView.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
