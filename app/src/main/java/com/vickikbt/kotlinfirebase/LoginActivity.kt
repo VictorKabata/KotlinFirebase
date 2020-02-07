b35243fb9af8c7ba0e9086f139159d958de73bbf
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-07 18:53:36 +0300

    Added databinding to the project and the available activities

--- app/src/main/java/com/vickikbt/kotlinfirebase/LoginActivity.kt
+++ app/src/main/java/com/vickikbt/kotlinfirebase/LoginActivity.kt
@@ -1,12 +1,18 @@
 package com.vickikbt.kotlinfirebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.vickikbt.kotlinfirebase.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+
+
     }
 }
--- app/src/main/java/com/vickikbt/kotlinfirebase/RegisterActivity.kt
+++ app/src/main/java/com/vickikbt/kotlinfirebase/RegisterActivity.kt
@@ -2,11 +2,15 @@ package com.vickikbt.kotlinfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.vickikbt.kotlinfirebase.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_register)
     }
 }
