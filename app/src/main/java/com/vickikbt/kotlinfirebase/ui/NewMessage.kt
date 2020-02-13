639d795406de55b3b36ec65febe2f46aebbab137
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-13 21:20:16 +0300

    Update NewMessage.kt

--- app/src/main/java/com/vickikbt/kotlinfirebase/ui/NewMessage.kt
+++ app/src/main/java/com/vickikbt/kotlinfirebase/ui/NewMessage.kt
@@ -1,27 +1,49 @@
 package com.vickikbt.kotlinfirebase.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.vickikbt.kotlinfirebase.R
 import com.vickikbt.kotlinfirebase.adapter.NewMessageViewHolder
+import com.vickikbt.kotlinfirebase.model.Users
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessage : AppCompatActivity() {
 
+    val newMessageViewHolder = NewMessageViewHolder()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
-
         supportActionBar?.title = "Select User"
-        val newMessageViewHolder = NewMessageViewHolder()
-        val adapter = GroupAdapter<ViewHolder>()
 
-        adapter.add(newMessageViewHolder)
-        adapter.add(newMessageViewHolder)
-        adapter.add(newMessageViewHolder)
-        recyclerview_new_message.adapter = adapter
+        fetchUser()
+    }
+
+    private fun fetchUser() {
+        val databaseRef = FirebaseDatabase.getInstance().getReference("/Users")
+        databaseRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                val adapter = GroupAdapter<ViewHolder>()
+
+                p0.children.forEach {
+                    Log.e("New Message", it.toString())
+                    val user = it.getValue(Users::class.java)
+                    adapter.add(newMessageViewHolder)
+                }
+                recyclerview_new_message.adapter = adapter
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        })
     }
 }
 
