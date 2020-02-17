4b0f920bd44d0666c03dd26730499564100d74e5
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-02-17 16:37:13 +0300

    Update NewMessage.kt

--- app/src/main/java/com/vickikbt/kotlinfirebase/ui/NewMessage.kt
+++ app/src/main/java/com/vickikbt/kotlinfirebase/ui/NewMessage.kt
@@ -3,17 +3,21 @@ package com.vickikbt.kotlinfirebase.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.vickikbt.kotlinfirebase.R
 import com.vickikbt.kotlinfirebase.databinding.ActivityNewMessageBinding
 
 class NewMessage : AppCompatActivity() {
 
     lateinit var binding: ActivityNewMessageBinding
+    lateinit var databaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_new_message)
         supportActionBar?.title = "Select User"
+        databaseRef = FirebaseDatabase.getInstance().getReference("Users")
 
         fetchUser()
     }
